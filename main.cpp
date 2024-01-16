56e66c4082d1ff642337ac20a4445676e2ccd772
Igorbraziel <reisbrazieligor@gmail.com>
2024-01-16 17:21:27 -0300

    mudanças

--- main.cpp
+++ main.cpp
@@ -7,12 +7,11 @@
 int main(){
     Estoque * est = CriarEstoque();
     Fila * fila_pedidos = CriarFila();
-    Pilha * pilha_devolucao = CriarPilha();
+    Pilha * pilha_reabastecimento = CriarPilha();
     char nome[TAM_MAX];
     int opcao;
 
     // inicialização do estoque ==================
-    /*
     strcpy(nome, "Laranja");
     AdicionarEstoque(est, CriarProduto(1, 56, nome));
 
@@ -22,6 +21,7 @@ int main(){
     strcpy(nome, "Uva");
     AdicionarEstoque(est, CriarProduto(4, 76, nome));
 
+    /*
     strcpy(nome, "Abacate");
     AdicionarEstoque(est, CriarProduto(16, 203, nome));
 
@@ -51,10 +51,10 @@ int main(){
     // inicialização devolucoes
 
     strcpy(nome, "Pirulito");
-    AdicionarProdutoPilha(pilha_devolucao, CriarProduto(104, 203, nome));
+    AdicionarProdutoPilha(pilha_reabastecimento, CriarProduto(104, 203, nome));
 
     strcpy(nome, "Chiclete");
-    AdicionarProdutoPilha(pilha_devolucao, CriarProduto(111, 453, nome));
+    AdicionarProdutoPilha(pilha_reabastecimento, CriarProduto(111, 453, nome));
 
     // =============================================
 
@@ -62,49 +62,47 @@ int main(){
 
     while(true){
         std::cout << VERMELHO << FUNDO_CINZA << "CENTRAL DE GERENCIAMENTO" << LIMPA_FUNDO << "\n\n";
-        std::cout << CIANO << "[1] -" << LIMPA << "(Adicionar Produto ao Estoque)\n";
-        std::cout << CIANO << "[2] -" << LIMPA << "(Adicionar Pedido na Fila)\n";
-        std::cout << CIANO << "[3] -" << LIMPA << "(Adicionar Produto na Pilha de Reabastecimento)\n";
-        std::cout << CIANO << "[4] -" << LIMPA << "(Atender Pedido da Fila)\n";
-        std::cout << CIANO << "[5] -" << LIMPA << "(Reabastecer Estoque com Itens da Pilha)\n";
-        std::cout << CIANO << "[6] -" << LIMPA << "(Visualizar Estoque)\n";
-        std::cout << CIANO << "[7] -" << LIMPA << "(Visualizar Fila de Pedidos)\n";
-        std::cout << CIANO << "[8] -" << LIMPA << "(Visualizar Pilha de Itens)\n";
-        std::cout << CIANO << "[9] -" << LIMPA << "(Finalizar o Programa)\n\n";
+        std::cout << CIANO << "[1] -" << LIMPA << "(Adicionar Pedido na Fila)\n";
+        std::cout << CIANO << "[2] -" << LIMPA << "(Adicionar Produto na Pilha de Reabastecimento)\n";
+        std::cout << CIANO << "[3] -" << LIMPA << "(Atender Pedido da Fila)\n";
+        std::cout << CIANO << "[4] -" << LIMPA << "(Reabastecer Estoque com Itens da Pilha)\n";
+        std::cout << CIANO << "[5] -" << LIMPA << "(Visualizar Estoque)\n";
+        std::cout << CIANO << "[6] -" << LIMPA << "(Visualizar Fila de Pedidos)\n";
+        std::cout << CIANO << "[7] -" << LIMPA << "(Visualizar Pilha de Itens)\n";
+        std::cout << CIANO << "[8] -" << LIMPA << "(Finalizar o Programa)\n\n";
         std::cout << AZUL << "Digite o número da opção desejada: " << LIMPA;
         std::cin >> opcao;
         std::cout << "\n";
 
-        if(opcao == 1){
-            AdicionarEstoque(est, MaquinaProdutos());
-            sleep(3);
-            std::cout << "\n";
-        } else if (opcao == 2){
+        if (opcao == 1){
             AdicionarPedidoFinal(fila_pedidos, MaquinaPedidos());
             sleep(3);
             std::cout << "\n";
-        } else if (opcao == 3){
-            AdicionarProdutoPilha(pilha_devolucao, MaquinaProdutos());
+        } else if (opcao == 2){
+            AdicionarProdutoPilha(pilha_reabastecimento, MaquinaProdutos());
             sleep(3);
             std::cout << "\n";
-        } else if (opcao == 4){
+        } else if (opcao == 3){
             AtendePedidoFila(est, fila_pedidos);
             sleep(3);
             std::cout << "\n";
-        } else if (opcao == 5){
-            ReabastecerEstoque(est, pilha_devolucao); // ESTÁ TENDO FALHAS
+        } else if (opcao == 4){
+            ReabastecerEstoque(est, pilha_reabastecimento); // ESTÁ TENDO FALHAS
             sleep(3);
             std::cout << "\n";
-        } else if (opcao == 6){
+        } else if (opcao == 5){
             MostrarEstoque(est);
-            sleep(3);
-        } else if (opcao == 7){
+            std::cout << std::endl;
+            sleep(5);
+        } else if (opcao == 6){
             MostrarFila(fila_pedidos);
-            sleep(3);
+            std::cout << std::endl;
+            sleep(5);
+        } else if (opcao == 7){
+            MostrarPilha(pilha_reabastecimento);
+            std::cout << std::endl;
+            sleep(5);
         } else if (opcao == 8){
-            MostrarPilha(pilha_devolucao);
-            sleep(3);
-        } else if (opcao == 9){
             std::cout << FUNDO_MAGENTA << "VOLTE SEMPRE !!!" << LIMPA_FUNDO << "\n\n";
             sleep(3);
             break;
@@ -116,7 +114,7 @@ int main(){
 
     DestruirEstoque(est);
     DestruirFila(fila_pedidos);
-    DestruirPilha(pilha_devolucao);
+    DestruirPilha(pilha_reabastecimento);
     
     return 0;
 }
